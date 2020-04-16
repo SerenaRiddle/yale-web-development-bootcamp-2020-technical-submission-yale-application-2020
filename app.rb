bde44eff54afcf0dcf1f11f3c637502a80c4497d
Serena Riddle <serena.riddle@yale.edu>
2020-04-16 21:29:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ age = 20
 favorite_food = "soup"
 height_in_inches = 71
 height_message = height_in_inches>AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
