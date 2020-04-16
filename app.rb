6cb3a05142cdd7928752fc79a988f0a6d60c0d6b
Serena Riddle <serena.riddle@yale.edu>
2020-04-16 21:30:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = "Serena"
 age = 20
 favorite_food = "soup"
-height_in_inches = 67
+height_in_inches = 71
 height_message = height_in_inches>AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
