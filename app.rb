7aaae4c72a9f059a242b220243291bda8b3552db
Serena Riddle <serena.riddle@yale.edu>
2020-04-16 21:26:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
-
+name = "Serena"
+age = 20
+favorite_food = "soup"
+height_in_inches = 71
+height_message = 
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
